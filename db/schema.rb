f5ba0e28efa08aa645c0c8db4bdb0c3ff9390053
joshneipp <joshneipp@gmail.com>
2015-10-01 17:56:07 -0400

    Merge steps and eights.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001212821) do
+ActiveRecord::Schema.define(version: 20151001212838) do
+
+  create_table "steps", force: :cascade do |t|
+    t.integer  "steps_taken"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "weights", force: :cascade do |t|
     t.string   "weight"
