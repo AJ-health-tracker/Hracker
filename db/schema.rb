d01e988a63ca03149ba2eafcd3f8ea7ae018c440
joshneipp <joshneipp@gmail.com>
2015-10-02 12:06:19 -0400

    Migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001212838) do
+ActiveRecord::Schema.define(version: 20151002155808) do
+
+  create_table "exercise_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "steps", force: :cascade do |t|
     t.integer  "steps_taken"
