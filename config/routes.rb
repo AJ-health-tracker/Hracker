9f4e29e26c8558a978286ce6a9277b16027afe98
Ashley Mason <amason@spoonflower.com>
2015-10-02 09:55:12 -0400

    created dashboards controller and modified root route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'dashboards/index'
+
+  get 'dashboards/show'
+
   resources :weights
   resources :steps
+  root 'dashboards#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
