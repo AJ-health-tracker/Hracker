4dd2cf01ecac941e99e5ba5d48b5ecc05f63c9db
joshneipp <joshneipp@gmail.com>
2015-10-02 15:48:41 -0400

    Add exercise controller actions.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,17 +1,36 @@
 class ExercisesController < ApplicationController
+  before_action :set_exercise, only: [:show, :edit, :update, :destroy]
+
   def index
     @exercises = Exercise.all
   end
 
   def show
+
   end
 
   def new
     @exercise = Exercise.new
-    @exercise_types = ExerciseType.all
-    @cardio = ExerciseType.create(name: 'Cardio')
-    @water_sports = ExerciseType.create(name: 'Water sports')
-    @weights = ExerciseType.create(name: 'Weights')
-    @flexibility = ExerciseType.create(name: 'Flexibility')
   end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+
+    if @exercise.save
+      redirect_to @exercise, notice: 'Exercise was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def set_exercise
+      @exercise = Exercise.find(params[:id])
+    end
+
+    def exercise_params
+      params.require(:exercise).permit(:exercise_types, :calories_burned)
+    end
+
 end
